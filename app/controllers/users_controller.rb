eeccd1906e86b485b3ec65274f3a2fd8a1ce8fb2
yikezawa <y.ikezawa93@gmail.com>
2021-10-16 20:12:17 +0900

    rt PATH=/Users/yikezawa/.anyenv/envs/goenv/versions/1.16.3/bin:/Users/yikezawa/Downloads/google-cloud-sdk/bin:/Users/yikezawa/.anyenv/envs/pyenv/shims:/Users/yikezawa/.anyenv/envs/pyenv/bin:/Users/yikezawa/.anyenv/envs/goenv/bin:/Users/yikezawa/.anyenv/bin:/usr/local/opt/mysql@5.6/bin:/Users/yikezawa/.cargo/bin:/Library/Frameworks/Python.framework/Versions/3.8/bin:/Users/yikezawa/.nodebrew/current/bin:/usr/local/var/nodebrew/current/bin:/Users/yikezawa/.rbenv/bin:/Applications/Postgres.app/Contents/Versions/11/bin:/Users/yikezawa/.rbenv/shims:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/yikezawa/Downloads/google-cloud-sdk/bin:/Users/yikezawa/.anyenv/envs/pyenv/shims:/Users/yikezawa/.anyenv/envs/pyenv/bin:/Users/yikezawa/.anyenv/envs/goenv/shims:/Users/yikezawa/.anyenv/envs/goenv/bin:/Users/yikezawa/.anyenv/bin:/usr/local/opt/mysql@5.6/bin:/Users/yikezawa/.cargo/bin:/Library/Frameworks/Python.framework/Versions/3.8/bin:/Users/yikezawa/.nodebrew/current/bin:/usr/local/var/nodebrew/current/bin:/Users/yikezawa/.rbenv/bin:/Applications/Postgres.app/Contents/Versions/11/bin:/Users/yikezawa/.rbenv/shims:/Users/yikezawa/go/bin:/Users/yikezawa/.local/bin:/Users/yikezawa/go/bin:/Users/yikezawa/.local/bin clear m

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_paramas
+  def user_params
     params.require(:user).permit(:name, :email, :password,
       :password_confirmation)
   end
