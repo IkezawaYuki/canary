6f10966cfbfbb710674155e90f8d4dd382c37884
yikezawa <y.ikezawa93@gmail.com>
2021-10-31 22:19:24 +0900

    mod

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,17 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.parmanent.signed[:user_id] = user.id
+    cookies.parmanent[:remember_token] = user.remember_token
+  end
+
   def current_user
-    if session[:user_id]
-      @user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      
     end
   end
 
