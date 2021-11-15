6ec7d222355f60eb7bc2d234ea4637e98942c2fc
yikezawa <y.ikezawa93@gmail.com>
2021-11-15 22:38:26 +0900

    mod

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template "users/edit"
     patch user_path(@user), params: { user: { name: "",
@@ -16,8 +17,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
-    assert_template 'user/edit'
+    assert_template 'users/edit'
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
@@ -25,7 +27,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
       password: "",
       password_confirmation: "" }}
     assert_not flash.empty?
-    assert_redirect_to @user
+    assert_redirected_to @user
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
