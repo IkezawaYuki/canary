192ac09184107125d02d6ee1a6a32bcc5bfaf309
yikezawa <y.ikezawa93@gmail.com>
2021-11-18 22:44:31 +0900

    mod

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirect_to login_url
+  end
 end
